226a7e47ea70f3d53a93a372082876bd1bee283d
Ayush Agrwal <ayushagarwal22@gmail.com>
2018-02-18 16:07:35 +0530

    Car class

--- eclipse-workspace/ParkingLot/src/com/gojek/parkinglot/Car.java
+++ eclipse-workspace/ParkingLot/src/com/gojek/parkinglot/Car.java
@@ -2,4 +2,21 @@ package com.gojek.parkinglot;
 
 public class Car {
 
+	private String colour;
+	private int registrationNumber;
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	public int getRegistrationNumber() {
+		return registrationNumber;
+	}
+	public void setRegistrationNumber(int registrationNumber) {
+		this.registrationNumber = registrationNumber;
+	}
+	
+	
+	
 }
